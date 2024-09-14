b84a6fbe57c2d273d7bdc20c2d71d0b9b66e3553
Santiago Castro <s.csanmiguel@hotmail.com>
2024-09-14 11:14:24 -0500

    Included transformador and bateria

--- function_app.py
+++ function_app.py
@@ -44,7 +44,7 @@ def cron_PAS800(myTimer: func.TimerRequest) -> None:
         sftp.close()
         transport.close()
 
-def read_flexible_csv(file_handle):
+def read_flexible_csv(file_handle,filename):
     # Read from file handle and decode as necessary
     content = file_handle.read().decode('utf-8')
     columns = [
@@ -57,7 +57,7 @@ def read_flexible_csv(file_handle):
     data = []
     metadata = {}
     reader = csv.reader(content.splitlines())
-    #print(reader)
+    print(reader)
     for i, row in enumerate(reader):
         if i == 1:  # First line contains some metadata
             metadata = {
@@ -69,32 +69,30 @@ def read_flexible_csv(file_handle):
         elif i == 6:  # The header line we need is the 7th line (index 6)
             continue
         elif i > 6:  # Process rows after the header
-            #if len(row) >= (len(columns) - 4 + 2):  # Since we already have first 4 columns from metadata
-            new_row = [
-                metadata.get("Gateway SN", None),
-                metadata.get("Device Name", None),
-                metadata.get("Device Local ID", None),
-                metadata.get("Device Type ID", None)
-            ] + row[2:len(columns) - 2]
+            if "MONTACARGAS" in filename:
+                new_row = [
+                    metadata.get("Gateway SN", None),
+                    metadata.get("Device Name", None),
+                    metadata.get("Device Local ID", None),
+                    metadata.get("Device Type ID", None),
+                    row[2],
+                    " "
+                ] + row[3:len(columns) - 2]
+            else:
+                new_row = [
+                    metadata.get("Gateway SN", None),
+                    metadata.get("Device Name", None),
+                    metadata.get("Device Local ID", None),
+                    metadata.get("Device Type ID", None)
+                ] + row[2:len(columns) - 2]
+            
             data.append(new_row)
-    
+            # Debugging: Print the data appended to ensure it's correct
+            #print("Appended row:", new_row)
+        
     # Create DataFrame
     df = pd.DataFrame(data, columns=columns)
-    #print(df)
-
-    # Replace NaN with 0 for all numeric columns
-    numeric_columns = [
-        "RmsFrequency", "RmsCurrentPhsA", "RmsCurrentPhsB", "RmsCurrentPhsC",
-        "TotalRmsPowerFactor", "RmsVoltagePhsAB", "RmsVoltagePhsBC", "RmsVoltagePhsCA",
-        "RmsActivePowerPhsA", "RmsActivePowerPhsB", "RmsActivePowerPhsC",
-        "TotalDeliveredActiveEnergy", "TotalRmsActivePower", 
-    ]
-    df[numeric_columns] = df[numeric_columns].apply(pd.to_numeric, errors='coerce').fillna(0)
-
-    #division of Energy and power to K
-    df["TotalDeliveredActiveEnergy"] = df["TotalDeliveredActiveEnergy"] / 1000
-    df["TotalRmsActivePower"] = df["TotalRmsActivePower"] / 1000
-    return df
+    print(df)
 
 # read flexible Csv2 only when origin file is DOSIFICADO (as it has a different structure)
 def read_flexible_csv2(file_handle):
@@ -156,15 +154,89 @@ def read_flexible_csv2(file_handle):
     df["TotalRmsActivePower"] = df["TotalRmsActivePower"] / 1000
     return df
 
+# read flexible Csv3 only when origin file is Transformador (as it has a different structure)
+def read_flexible_csv3(file_handle):
+    # Read from file handle and decode as necessary
+    content = file_handle.read().decode('utf-8')
+    columns = [
+        "GatewaySN", "DeviceName", "DeviceLocalID", "DeviceTypeID", "LocalTimeStamp",
+        "RmsFrequency", "RmsCurrentPhsA", "RmsCurrentPhsB", "RmsCurrentPhsC",
+        "TotalRmsPowerFactor", "RmsVoltagePhsAB", "RmsVoltagePhsBC", "RmsVoltagePhsCA",
+        "RmsActivePowerPhsA", "RmsActivePowerPhsB", "RmsActivePowerPhsC",
+        "TotalDeliveredActiveEnergy", "TotalRmsActivePower", "RmsMaxCurrentPhsC","RmsCurrentNeut" 
+    ]
+    data = []
+    metadata = {}
+    reader = csv.reader(content.splitlines())
+    # mapping Transformador
+    input_to_output_map = {
+        0: 4,   # Local Time Stamp
+        3: 5,   # RmsFrequency (Hz)
+        4: 6,   # RmsCurrentPhsA (A)
+        6: 7,   # RmsCurrentPhsB (A)
+        7: 8,   # RmsCurrentPhsC (A)
+        10: 9,  # TotalRmsPowerFactor
+        20: 10, # RmsVoltagePhsAB (V)
+        33: 11, # RmsVoltagePhsBC (V)
+        35: 12, # RmsVoltagePhsCA (V)
+        38: 13, # RmsActivePowerPhsA (W)
+        39: 14, # RmsActivePowerPhsB (W)
+        40: 15, # RmsActivePowerPhsC (W)
+        42: 16, # TotalDeliveredActiveEnergy (Wh)
+        44: 17, # TotalRmsActivePower (W)
+        9: 19   # RmsCurrentNeut (A)
+    }
+
+    for i, row in enumerate(reader):
+        if i == 1:  # First line contains some metadata
+            metadata = {
+                "Gateway SN": row[1],
+                "Device Name": row[4],
+                "Device Local ID": row[5],
+                "Device Type ID": row[6]
+            }
+        elif i == 6:  # The header line we need is the 7th line (index 6)
+            continue
+        elif i > 6:  # Process rows after the header
+            # Initialize a row with metadata and zeros for all data columns
+            new_row = [
+                metadata.get("Gateway SN", None),
+                metadata.get("Device Name", None),
+                metadata.get("Device Local ID", None),
+                metadata.get("Device Type ID", None)
+            ] + [0] * (len(columns) - 4)
+
+             # Map the input row values to the new row based on the mapping
+            for input_idx, output_idx in input_to_output_map.items():
+                # Adjust index because input starts from column 2 (indexing from 0)
+                if input_idx + 2 < len(row):  # Ensure the index is within the row length
+                    new_row[output_idx] = row[input_idx + 2]  # Map the value to the correct position
+
+            # Append the newly structured row to the data list
+            data.append(new_row)
+        
+    # Create DataFrame
+    df = pd.DataFrame(data, columns=columns)
+
+    # Replace NaN with 0 for all numeric columns
+    numeric_columns = [
+        "RmsFrequency", "RmsCurrentPhsA", "RmsCurrentPhsB", "RmsCurrentPhsC",
+        "TotalRmsPowerFactor", "RmsVoltagePhsAB", "RmsVoltagePhsBC", "RmsVoltagePhsCA",
+        "RmsActivePowerPhsA", "RmsActivePowerPhsB", "RmsActivePowerPhsC",
+        "TotalDeliveredActiveEnergy", "TotalRmsActivePower","RmsMaxCurrentPhsC","RmsCurrentNeut" 
+    ]
+    df[numeric_columns] = df[numeric_columns].apply(pd.to_numeric, errors='coerce').fillna(0)
+    return df
+
 def is_dosificado_file(filename):
     # Use a regular expression to check if 'DOSIFICADO' is exactly part of the filename pattern you expect
     pattern = r'_DOSIFICADO_'  # \b represents a word boundary in regex
     return re.search(pattern, filename, re.IGNORECASE) is not None
 
-def should_exclude_file(filename):
-    # Patterns for 'transformador' or 'montacargas'
-    exclude_patterns = [r'Transformador', r'MONTACARGAS']
-    return any(re.search(pattern, filename, re.IGNORECASE) for pattern in exclude_patterns)
+def is_transformador_file(filename):
+    # Use a regular expression to check if 'Transformador' is exactly part of the filename pattern you expect
+    pattern = r'Transformador'  # \b represents a word boundary in regex
+    return re.search(pattern, filename, re.IGNORECASE) is not None
     
 def fetch_recent_files(sftp, minutes):
     # Get the current time
@@ -196,9 +268,9 @@ def batch_process_files(sftp):
         #logging.info(f"Checking file: {file}")  # Log the file being checked  
         with sftp.open(file, mode='r') as file_handle:
             # Determine which function to use based on file name
-            if should_exclude_file(file):
-                logging.info(f"Skipping file due to filter: {file}")
-                continue
+            if is_transformador_file(file):
+                logging.info(f"Processing Transformador file: {file}")
+                df = read_flexible_csv3(file_handle)
             elif is_dosificado_file(file):
                 logging.info(f"Processing DOSIFICADO file: {file}")
                 df = read_flexible_csv2(file_handle)
